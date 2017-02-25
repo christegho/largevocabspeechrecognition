7d01e8a5b0f44d04caae5154a811865a4aa0c544
Chris Tegho <christegho@users.noreply.github.com>
2017-02-25 13:15:27 +0000

    Add files via upload

--- minEditAlign.py
+++ minEditAlign.py
@@ -1,5 +1,6 @@
 import numpy as np
 import operator
+import argparse
 #import tabulate as tb
 #sourse http://www.giovannicarmantini.com/2016/01/minimum-edit-distance-in-python
 
@@ -7,7 +8,7 @@ import operator
 def wagner_fischer(word_1, word_2, timing_1, timing_2):
     n = len(word_1) + 1  # counting empty string 
     m = len(word_2) + 1  # counting empty string
- 
+    timalign = 0.2
     # initialize D matrix
     D = np.zeros(shape=(n, m), dtype=np.int)
     D_ = np.zeros(shape=(n, m), dtype=np.int)
@@ -33,7 +34,7 @@ def wagner_fischer(word_1, word_2, timing_1, timing_2):
 	    if (timing_1[i-1][0]<timing_2[j-1][1] and timing_2[j-1][0]<timing_1[i-1][1]):
 		e2t2 = timing_2[j-1][1]-timing_2[j-1][0]
 		e1t1 = timing_1[i-1][1]-timing_1[i-1][0]
-		if (e2t2 > 0.20*e1t1 or e2t2 < 1.8*e1t1):
+		if (e2t2 > timalign*e1t1 or e2t2 < (2-timalign)*e1t1):
 			ct = 0
 			idt = 0
 			aligned = True
@@ -84,7 +85,7 @@ def naive_backtrace(B_matrix):
     return backtrace_idxs
 
 
-def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score):
+def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score, alpha):
  
     aligned_word_1 = []
     aligned_word_2 = []
@@ -120,9 +121,9 @@ def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score
                 w_2_letter = word_2[j_0]
 		tim = timing_1[i_0]
                 op = "s"
-		scoresRecomp1.append((scores_1[i_0])/2.0)
-		scoresRecomp2.append((scores_2[j_0])/2.0)
-		values = [(scores_1[i_0])/2.0,(scores_2[j_0])/2.0]
+		scoresRecomp1.append(alpha*(scores_1[i_0]))
+		scoresRecomp2.append((1-alpha)*(scores_2[j_0]))
+		values = [alpha*(scores_1[i_0]),(1-alpha)*(scores_2[j_0])]
 		arcs = [word_1[i_0],word_2[j_0]]
 		max_index, max_value = max(enumerate(values), key=operator.itemgetter(1))
 		best1['score'].append(max_value)
@@ -133,8 +134,8 @@ def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score
             op = "i"
 	    tim = timing_2[j_0]
 	    scoresRecomp1.append(none_score)
-	    scoresRecomp2.append((scores_2[j_0])/2.0)
-	    values = [none_score,(scores_2[j_0])/2.0]
+	    scoresRecomp2.append((1-alpha)*(scores_2[j_0]))
+	    values = [none_score,(1-alpha)*(scores_2[j_0])]
 	    arcs = ['!NULL',word_2[j_0]]
   	    max_index, max_value = max(enumerate(values), key=operator.itemgetter(1))
 	    best1['score'].append(max_value)
@@ -145,9 +146,9 @@ def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score
             w_2_letter = " "
             op = "d"
 	    tim = timing_1[i_0]
-	    scoresRecomp1.append((scores_1[i_0])/2.0)
+	    scoresRecomp1.append(alpha*(scores_1[i_0]))
 	    scoresRecomp2.append(none_score)
-	    values = [(scores_1[i_0])/2.0,none_score]
+	    values = [alpha*(scores_1[i_0]),none_score]
 	    arcs = [word_1[i_0], '!NULL']
   	    max_index, max_value = max(enumerate(values), key=operator.itemgetter(1))
 	    best1['score'].append(max_value)
@@ -160,64 +161,78 @@ def align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score
 	timing.append(tim)
  
 	#Time readjustment and score averaging
-	for op in range(len(operations)):
-		if operations[op] == 'i' :
+	for op in range(1):#range(len(operations)):
+		if operations[op] == 'g' :#'i'
 			timeI = timing[op]
 			if op != 0 and timeI[0] <= timing[op-1][1]:
-				timing[op][0] = timing[op-1][1]+1
+				timing[op][0] = timing[op-1][1]
 			if op != len(operations)-1 and timeI[1] >= timing[op+1][0]:
-				if timing[op+1][0] > timing[op][0]+1:
-					timing[op][1] = timing[op+1][0]-1
+				if timing[op+1][0] > timing[op][0]:
+					timing[op][1] = timing[op+1][0]
 				else:
-					timing[op+1][0] = max(timing[op][1]+1,timing[op][0]+1)
-					timing[op][1] = timing[op+1][0]-1
+					timing[op+1][0] = max(timing[op][1],timing[op][0])
+					timing[op][1] = timing[op+1][0]
 		
     return aligned_word_1, aligned_word_2, operations, timing, scoresRecomp1, scoresRecomp2, best1
 
 
 
 def test():
-	#from minEditAlign import *
-	print 'test'
+	timing_2 = [[1,3],[4,7],[8,10],[11,14]]
+	timing_1 = [[4,5],[6,8],[9,14],[15,17],[18,20]]
+	word_2 = ["jo", "lo", "po","mo"]
+	word_1 = ["jo", "po","mo","to","ro"]
 
-def extractMLF():
-filenames = ['/grph-hybrid-bg/grph-tandem-merge-bg/dev03_DEV001-20010117-XX2000/decode/rescore.mlf','/grph-tandem-bg/grph-plp-merge-bg/PR4000.0/dev03_DEV001-20010117-XX2000/decode/rescore.mlf']
-mlf = [];
-mlfDet = [];
-none_score = 0;
-indir1 = '/remote/mlsalt-2016/ct506/MLSALT11'+ filenames[0]
-indir2 = '/remote/mlsalt-2016/ct506/MLSALT11'+ filenames[1]
-text1 = open(indir1).read();
-text2 = open(indir2).read();
-recs1 = text1.split('.rec"\n')
-recs2 = text2.split('.rec"\n')
-mlf.append('#!MLF!#\n')
+	word_2 = ["I", "am", "not","mean"]
+	word_1 = ["I", "am","mean","and","gross"]
 
-mlf.append('"*'+recs1[0].split('lattices')[1]+'.rec"\n')
-mlfDet.append('*'+recs1[0].split('lattices')[1]+'\n')
-for i in range(1, len(recs1)):
-		words1 = recs1[i].split('.\n')[0].split('\n')
-		words_1, scores_1, timing_1 = extractHyp(words1)
-		words2 = recs2[i].split('.\n')[0].split('\n')
-		words_2, scores_2, timing_2 = extractHyp(words2)
-		D, B = wagner_fischer(words_1, words_2, timing_1, timing_2)
-		bt = naive_backtrace(B)	 
-		aligned_word_1, aligned_word_2, operations, timing, scoresRecomp1, scoresRecomp2, best1 = align(words_1, words_2, bt, timing_1, timing_2, scores_1, scores_2, none_score)
-		for besti in range(len(best1['seq'])):
-			mlf.append(str(int(timing[besti][0])) + ' ' + str(int(timing[besti][1])) + ' ' + best1['seq'][besti] + ' ' + str(best1['score'][besti]) + '\n')
-			mlfDet.append( aligned_word_1[besti] + ' ' + aligned_word_2[besti] + ' ' + str(scoresRecomp1[besti]) + ' '+ str(scoresRecomp2[besti]) + ' '+ operations[besti] + '\n')
-		if i < len(recs1)-1:		
-			mlf.append('.\n"*'+recs1[i].split('.\n')[1].split('lattices')[1]+'.rec"\n')
-			mlfDet.append('*'+recs1[i].split('.\n')[1].split('lattices')[1]+'\n')
+	D, B = wagner_fischer(word_1, word_2, timing_1, timing_2)
+	bt = naive_backtrace(B)
+		 
+	scores_2 = [0.7, 0.2, 0.7,1.0]
+	scores_1 = [0.1,0.8,0.6,1.0,0.6]
+	none_score = 0.2
+	alignment_table = align(word_1, word_2, bt, timing_1, timing_2, scores_1, scores_2, none_score, 0.5)
+	print alignment_table
 
-mlf.append('.')
-mlfFile = open('rescore.mlf', 'w')
-for item in mlf:
-  	mlfFile.write("%s" % item)
+def extractMLF(alpha, noneSc):
+	filenames = ['/grph-hybrid-bg/grph-tandem-merge-bg/dev03_DEV001-20010117-XX2000/decode/rescore.mlf','/grph-tandem-bg/grph-plp-merge-bg/PR4000.0/dev03_DEV001-20010117-XX2000/decode/rescore.mlf']
+	mlf = [];
+	mlfDet = [];
+	none_score = noneSc;
+	indir1 = '/remote/mlsalt-2016/ct506/MLSALT11'+ filenames[0]
+	indir2 = '/remote/mlsalt-2016/ct506/MLSALT11'+ filenames[1]
+	text1 = open(indir1).read();
+	text2 = open(indir2).read();
+	recs1 = text1.split('.rec"\n')
+	recs2 = text2.split('.rec"\n')
+	mlf.append('#!MLF!#\n')
 
-mlfFile2 = open('rescore2.mlf', 'w')
-for item in mlfDet:
-  	mlfFile2.write("%s" % item)
+	mlf.append('"*'+recs1[0].split('lattices')[1]+'.rec"\n')
+	mlfDet.append('*'+recs1[0].split('lattices')[1]+'\n')
+	for i in range(1, len(recs1)):
+			words1 = recs1[i].split('.\n')[0].split('\n')
+			words_1, scores_1, timing_1 = extractHyp(words1)
+			words2 = recs2[i].split('.\n')[0].split('\n')
+			words_2, scores_2, timing_2 = extractHyp(words2)
+			D, B = wagner_fischer(words_1, words_2, timing_1, timing_2)
+			bt = naive_backtrace(B)	 
+			aligned_word_1, aligned_word_2, operations, timing, scoresRecomp1, scoresRecomp2, best1 = align(words_1, words_2, bt, timing_1, timing_2, scores_1, scores_2, none_score, alpha)
+			for besti in range(len(best1['seq'])):
+				mlf.append(str(int(timing[besti][0])) + ' ' + str(int(timing[besti][1])) + ' ' + best1['seq'][besti] + ' ' + str(best1['score'][besti]) + '\n')
+				mlfDet.append( aligned_word_1[besti] + ' ' + aligned_word_2[besti] + ' ' + str(scoresRecomp1[besti]) + ' '+ str(scoresRecomp2[besti]) + ' '+ operations[besti] + '\n')
+			if i < len(recs1)-1:		
+				mlf.append('.\n"*'+recs1[i].split('.\n')[1].split('lattices')[1]+'.rec"\n')
+				mlfDet.append('*'+recs1[i].split('.\n')[1].split('lattices')[1]+'\n')
+
+	mlf.append('.')
+	mlfFile = open('/home/ct506/MLSALT11/copy/dev03_DEV001-20010117-XX2000/decode/rescore.mlf', 'w')
+	for item in mlf:
+	  	mlfFile.write("%s" % item)
+
+	mlfFile2 = open('rescore2.mlf', 'w')
+	for item in mlfDet:
+	  	mlfFile2.write("%s" % item)
 
 
 
@@ -231,3 +246,21 @@ def extractHyp(words1):
 				scores_1.append(float(hyps[3]))
 				timing_1.append([float(hyps[0]), float(hyps[1])])
 	return words_1, scores_1, timing_1
+
+
+
+def main() :
+    
+    parser = argparse.ArgumentParser(description='ROVER.')
+    parser.add_argument('--alpha', dest='alpha', action='store', default=0.5,
+                        help='alpha')
+    parser.add_argument('--none', dest='noneSc', action='store', default=-500,
+                        help='none score')
+   
+    args = parser.parse_args()
+    extractMLF(float(args.alpha), float(args.noneSc))
+
+    
+    
+if __name__ == '__main__':
+    main()
